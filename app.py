b27f24c9e9b763081065e1e5a90028ac5ae117f4
Jason Marquette <jasonmarquette@yahoo.com>
2025-07-04 15:29:27 -0500

    updated again for formatting.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def init_bedrock_agent_runtime_client():
     return boto3.client(
         "bedrock-agent-runtime",
         region_name="us-east-1",
-        config=Config(read_timeout=300)
+        config=Config(read_timeout=300),
     )
 
 
@@ -22,22 +22,21 @@ bedrock_client = init_bedrock_agent_runtime_client()
 
 app = Flask(__name__)
 
+
 @app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json() or {}
     user_prompt = data.get("prompt", "")
 
     input_data = {
-        "input": {
-            "text": user_prompt
-        },
+        "input": {"text": user_prompt},
         "retrieveAndGenerateConfiguration": {
             "type": "KNOWLEDGE_BASE",
             "knowledgeBaseConfiguration": {
                 "knowledgeBaseId": KNOWLEDGE_BASE_ID,
-                "modelArn": MODEL_ARN
-            }
-        }
+                "modelArn": MODEL_ARN,
+            },
+        },
     }
 
     try:
@@ -46,7 +45,7 @@ def chat():
         return jsonify({"response": bot_response})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
+
 
 if __name__ == "__main__":
     app.run(port=5000)
